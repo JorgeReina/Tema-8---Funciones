8ba43e8ee0e7216feab852128e115be812b2efdb
JorgeReina <reinaromerojorge@gmail.com>
2023-01-23 16:34:53 +0100

    Fichero.java

---
+++ Ejercicio1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+* 
+* Ejercicio 12 Bidimensional, Realiza un programa que muestre por 
+* pantalla un array de 9 filas por 9 columnas relleno con números 
+* aleatorios entre 500 y 900. A continuación, el programa debe mostrar 
+* los números de la diagonal que va desde la esquina inferior izquierda 
+* a la esquina superior derecha, así como el máximo, el mínimo y la 
+* media de los números que hay en esa diagonal.
+* 
+* Nombre del archivo: Ejercicio1.java
+* 
+* @author Jorge Reina Romero
+* 
+*/
+ 
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		
+		Scanner datos = new Scanner(System.in);
+		
+	}
+}
